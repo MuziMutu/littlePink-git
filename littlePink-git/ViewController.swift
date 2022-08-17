ba59749d1b8a98f7af171f025654b640b807fd22
MuziMutu <mzmt444444@163.com>
2022-08-17 21:56:14 +0800

    11223

--- littlePink-git/ViewController.swift
+++ littlePink-git/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("更新ui")
+        print("更新ui啦啦啦")
     }
 
 
