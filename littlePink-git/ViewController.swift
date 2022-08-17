c1bf345d23326b772d18df4e3e0ea1c3f252f0c5
MuziMutu <mzmt444444@163.com>
2022-08-17 22:04:05 +0800

    commit 123

--- littlePink-git/ViewController.swift
+++ littlePink-git/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("更新ui啦啦啦")
+        print("123")
     }
 
 
